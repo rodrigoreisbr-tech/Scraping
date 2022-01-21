277bf55d50e3b29c5e69ada97e4e7f8ed925b207
rodrigoreisbr-tech <rodrigo.reis12@gmail.com>
2022-01-21 13:34:13 -0800

    Initial Commit

---
+++ quotes/quotes/spiders/quotes_spider.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class QuotesSpide(scrapy.Spider):
+        name = "quotes"
+        start_urls = [
+            "https://quotes.toscrape.com/page/1/",
+            "https://quotes.toscrape.com/page/1/"
+        ]
+
+        def parse(self, response):
+            for quotes in response.css('div.quote'):
+               yield{
+                   'text':quotes.css('span.text::text').get(),
+                   'author': quotes.css('small.author::text').get(),
+                   'tags': quotes.css('div.tags a.tag::text').getall()
+               }
+
